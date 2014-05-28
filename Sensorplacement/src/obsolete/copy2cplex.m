6a882d1aa73bed8c31884f63f53905562cf09ee9
Nicolaj Kirchhof <nicolaj.kirchhof@gmail.com>
2014-05-29 01:43:55 +0200

    updated structure

--- Sensorplacement/src/obsolete/copy2cplex.m
+++
@@ -1,8 +0,0 @@
-function copy_cplex_fields(str, cplex)
-%%
-valid_fieldnames = {'A', 'ctype', 'obj', 'lb', 'ub', 'lhs', 'rhs', 'sense', 'rowname', 'colname'};
-for fn = fieldnames(str)'
-    if any(strcmpi(fn{1}, valid_fieldnames))
-        cplex.Model.(fn{1}) = str.(fn{1});
-    end
-end
