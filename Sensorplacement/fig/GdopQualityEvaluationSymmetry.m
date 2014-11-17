f929bf6ef430e05fe93f35d3f36bbe52c319fe5c
Nicolaj Kirchhof <nicolaj.kirchhof@gmail.com>
2014-11-17 02:28:37 +0100

    added gdop to figures

--- Sensorplacement/fig/GdopQualityEvaluationSymmetry.m
+++ Sensorplacement/fig/GdopQualityEvaluationSymmetry.m
@@ -1,12 +1,5 @@
-function [ qs ] = Qeval( x, y, x1, y1, x2, y2, dmax )
-%QEVAL Summary of this function goes here
-%   Detailed explanation goes here
-
- qs = sqrt((((x-x1)^2+(x-x2)^2-(x1-x2)^2+(y-y1)^2+(y-y2)^2-(y1-y2)^2)^2*...
+ qs = @( x, y, x1, y1, x2, y2, dmax ) sqrt((((x-x1)^2+(x-x2)^2-(x1-x2)^2+(y-y1)^2+(y-y2)^2-(y1-y2)^2)^2*...
      (-1.0/4.0))/(((x-x1)^2+(y-y1)^2)*((x-x2)^2+(y-y2)^2))+1.0)/((((x-x1)^2+(y-y1)^2)*((x-x2)^2+(y-y2)^2))/dmax+1.0);
-
-return;
-end
 
 %% plot contour
 x = linspace(50,250);
