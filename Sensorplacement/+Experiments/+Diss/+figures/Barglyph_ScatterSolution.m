0143e6349bc2de18cc6bd055d56e32bac0080178
NicolajKirchhof <nick@zuehlke.com>
2015-03-20 02:06:07 +0100

    Updated Plot creation

--- Sensorplacement/+Experiments/+Diss/+figures/Barglyph_ScatterSolution.m
+++ Sensorplacement/+Experiments/+Diss/+figures/Barglyph_ScatterSolution.m
@@ -17,6 +17,8 @@ for idn = 1:numel(opt_names)
     opt_name = opt_names{idn};
     if ~any(strcmp(opt_name, {'cmcqm_cmaes_it', 'cmcqm_nonlin_it'}))
         %%
+        mean_wpn_qualities = zeros(2601,1);
+        sp_selected_mat = zeros(2601,1);
         opt = opts.(opt_name);
         flt_eval = ~cellfun(@isempty, opt);
         
@@ -27,7 +29,7 @@ for idn = 1:numel(opt_names)
         wpn_qualities = cellfun(@(x) x.quality.sum_max/x.all_wpn, opt(flt_eval));
     
         ids = sub2ind([51, 51], num_sp/10+1, num_wpn/10+1);
-        mean_wpn_qualities(ids) = sp_selected;
+        mean_wpn_qualities(ids) = wpn_qualities;
         sp_selected_mat(ids) = sp_selected;
         all_mean_wpn_qualities(:, cnt)  = mean_wpn_qualities(:);
         all_num_sp_selected(:, cnt) = sp_selected_mat(:);
@@ -41,22 +43,74 @@ end
 range = 1:5:51;
 [idx, idy] = meshgrid(range, range);
 ind = sub2ind([51 51], idx(:), idy(:));
-%%%
-% x = X(range, range);
-% y = Y(range, range);
 
 xlist = X(ind);
 ylist = Y(ind);
-hlist = 10*ones(size(xlist));
-wlist = 30*ones(size(xlist));
-xrange = [-20 540];
+height = 20;
+width = 30;
+xrange = [-30 540];
 yrange = [-30 530];
 
-% graph_data = cellfun(@(x) x(range, range), all_mean_wpn_qualities, 'uniformoutput', false);
+all_x = unique(xlist);
+all_y = unique(ylist);
 
-barglyph(xlist, ylist, hlist, wlist, all_mean_wpn_qualities(ind, 1:5), xrange, yrange);
-% legend(opt_names{1:5});
-%%%
+%%
+close all;
+% h0 = barglyph(xlist, ylist, height, width, all_mean_wpn_qualities(ind, 1:5), xrange, yrange);
+% figure;
+% h0 = barglyph(xlist, ylist, height, width, all_mean_wpn_qualities(ind, [1:3, 5]), xrange, yrange);
+
+all_mean_wpn_qualities_rounded = round(all_mean_wpn_qualities*100);
+
+figure;
+h0 = barglyph(xlist, ylist, height, width, all_mean_wpn_qualities_rounded(ind, [1:3, 5]), xrange, yrange);
+
+figure;
+h0 = barglyph(xlist, ylist, height, width, all_mean_wpn_qualities_rounded(ind, [4,8:10]), xrange, yrange);
+
+figure;
+h0 = barglyph(xlist, ylist, height, width, all_num_sp_selected(ind, [1:3, 5]), xrange, yrange);
+
+figure;
+h0 = barglyph(xlist, ylist, height, width, all_num_sp_selected(ind, [4,8:10]), xrange, yrange);
+
+%%
+% inputs = arrayfun(@(wpn, sp) Experiments.Diss.small_flat(wpn, sp), all_x, all_y,'uniformoutput', false);
+% real_num_sp = cellfun(@(x) x.discretization.num_sensors, inputs);
+% real_num_wpn = cellfun(@(x) x.discretization.num_positions, inputs);
+
+
+strlabelsx = arrayfun(@(x, y) sprintf('%d (%d)', x, y), all_x, real_num_sp, 'uniformoutput', false);
+strlabelsy = arrayfun(@(x, y) sprintf('%d (%d)', x, y), all_y, real_num_wpn, 'uniformoutput', false);
+set(h0,  'xticklabel', strlabelsx, 'yticklabel', strlabelsy, 'Ticklength', [0 0], 'box', 'on');
+xlabel(h0,'\#$WPN$', 'interpreter', 'none');
+ylabel(h0, '\#$SP$', 'interpreter', 'none');
+
+
+
+
+
+
+%%
+% matlab2tikz('export\SmallFlatWpnCoverageNumSp.tikz', 'parseStrings', false,...
+%     'tikzFileComment', '% -*- root: TestingFigures.tex -*-', 'extraAxisOptions',{'y post scale=1'});
+
 matlab2tikz('export\SmallFlatWpnCoverageNumSp.tikz', 'parseStrings', false,...
-    'tikzFileComment', '% -*- root: TestingFigures.tex -*-', 'extraAxisOptions',{'y post scale=1'});
+    'tikzFileComment', '% -*- root: SmallFlatWpnCoverageNumSp.tex -*-',... 
+    'extraAxisOptions',{'y post scale=1',...
+    'every x tick label/.append style={tickwidth=0cm,major tick length=0cm}'});
+%     'standalone', true);
+%     'every outer x axis line/.append style={black}'});
+
+
+find_and_replace('export\SmallFlatWpnCoverageNumSp.tikz','bar width=0.025453in,', '');
+find_and_replace('export\SmallFlatWpnCoverageNumSp.tikz','bar shift=-0.063633in,', '');
+find_and_replace('export\SmallFlatWpnCoverageNumSp.tikz','bar shift=-0.031817in,', '');
+find_and_replace('export\SmallFlatWpnCoverageNumSp.tikz','bar shift=0.031817in,', '');
+find_and_replace('export\SmallFlatWpnCoverageNumSp.tikz','bar shift=0.063633in,', '');
+% find_and_replace('export\SmallFlatWpnCoverageNumSp.tikz','area legend,', 'major x tick style = transparent,');
+
+Figures.writeTexFile('SmallFlatWpnCoverageNumSp.tikz', 'export\');
+
+
 %%
--- Sensorplacement/barglyph.m
+++ Sensorplacement/barglyph.m
@@ -1,60 +1,43 @@
-function barglyph(xlist,ylist,hlist, wlist,graph_data, xrange, yrange)
+function h0 = barglyph(xlist,ylist,height, width,graph_data, xrange, yrange)
 %%
-% range = 1:5:size(X,1);
-% x = X(range, range);
-% y = Y(range, range);
-% v1 = num_sp_selected_gco(range, range);
-% v2 = num_sp_selected_gcss(range, range);
-% v3 = num_sp_selected_gsss(range, range);
-% 
-% xlist = x(:);
-% ylist = y(:);
-% slist = 20*ones(size(xlist));
-% xrange = [-10 510];
-% yrange = [-10 510];
-% 
-% graph_data = [v1(:), v1(:), v3(:)];
-
 canvas_range = [0.1 0.9];
 canvas_length = diff(canvas_range);
 
 xlength = diff(xrange);
 ylength = diff(yrange);
+h = height/(xlength);
+w = width/(ylength);
 
 data_max = max(graph_data(:));
+data_min = min(graph_data(graph_data>0));
 
 %%
 clf;
+all_x = unique(xlist);
+all_y = unique(ylist);
+
 h0 = axes('position',[canvas_range(1), canvas_range(1), canvas_length, canvas_length], ...
-    'xlim',xrange,'ylim',yrange, 'Visible', 'off');
+    'xlim',xrange,'ylim',yrange, 'ytick', all_y, 'xtick', all_x, 'Color', [1 1 1]);
+
+%%
 for i = 1:size(graph_data,1)
-h = hlist(i)/(xlength);
-w = wlist(i)/(ylength);
-x = (xlist(i)-xrange(1))/(xlength)*canvas_length+canvas_range(1)-h/2;
-y = (ylist(i)-yrange(1))/(ylength)*canvas_length+canvas_range(1)-w/2;
-
-%%
-% h1= axes('position',[x y w h], 'Visible', 'off', 'ycolor',get(gcf,'color'),'ytick',[]);
-h1= axes('position',[x y w h], 'ycolor',get(gcf,'color'),'ytick',[]);
-hold on;
-d = graph_data(i, :);
-% d = d-min(d);
-clne = linspace(0, 0.8, numel(d));
-cbar = [clne', clne', clne'];
-for idb = 1:numel(d)
-    bar(h1, idb, d(idb) , 'linestyle', 'none', 'facecolor', cbar(idb, :) );
-end
-title(sprintf('(%d, %d)', xlist(i), ylist(i)));  
-set(h1, 'XTick', 1:numel(d));
-strlabel = arrayfun(@(x) num2str(x), d, 'uniformoutput', false);
-set(h1, 'XTickLabel', strlabel);
-% axis on
-% axis off
-xlim([0.5 numel(d)+0.5]);
-ylim([0 data_max]);
+    
+    x = (xlist(i)-xrange(1))/(xlength)*canvas_length+canvas_range(1)-w/2;
+    y = (ylist(i)-yrange(1))/(ylength)*canvas_length+canvas_range(1)-h/2;
+        
+    %%
+    d = graph_data(i, :);
+    strlabel = arrayfun(@(x) num2str(x), d, 'uniformoutput', false);
+    h1= axes('position',[x y w h], 'ycolor',[0 0 0], 'xcolor',[0 0 0], 'ytick',[], 'Ticklength', [0 0],...
+        'xlim', [0.5 numel(d)+0.5], 'ylim', [data_min data_max], 'Box', 'on',...
+        'XTick', 1:numel(d), 'XTickLabel', strlabel);
+    hold on;
+    
+    clne = linspace(0, 0.8, numel(d));
+    cbar = [clne', clne', clne'];
+    for idb = 1:numel(d)
+        bar(h1, idb, d(idb) , 'linestyle', 'none', 'facecolor', cbar(idb, :), 'BarWidth', 0.8 );
+    end
 end
 
-xlim(h0, xrange);
-ylim(h0, yrange);
 return;
-%%
