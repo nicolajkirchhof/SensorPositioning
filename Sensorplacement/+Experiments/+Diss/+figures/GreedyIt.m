853b2c148815e0642827f655f3f4387ef335bb8e
NicolajKirchhof <nicolaj.kirchhof@zuehlke.com>
2015-04-14 00:31:03 +0200

    added git eval print

--- Sensorplacement/+Experiments/+Diss/+figures/GreedyIt.m
+++ Sensorplacement/+Experiments/+Diss/+figures/GreedyIt.m
@@ -30,6 +30,9 @@ for ideval = 1:numel(eval_names)
     end
     xlim([0 2600])
     ylabel('[\#]', 'interpreter', 'none');
+    if ideval == 1
+        set(gca, 'YTick', 0:2:8);
+    end
     
 
     %%
@@ -45,5 +48,23 @@ for ideval = 1:numel(eval_names)
     find_and_replace(full_filename,'bar\ shift=.\d.\d*cm,', '');
     find_and_replace(full_filename,'bar\ shift=\d.\d*cm,', '');
     find_and_replace(full_filename,'inner\ sep=0mm', 'inner sep=1pt');
-    Figures.compilePdflatex(filename, false, false);
+%     Figures.compilePdflatex(filename, false, false);
+end
+%%
+clc
+for ideval = 1:numel(eval_names)
+    %%
+    %     ideval = 1;
+    eval_name = eval_names{ideval};
+    gsp = all_eval.(eval_name).all_num_sp_selected(:, (3:5)) ;
+    gitsp = all_eval.(eval_name).greedy_it_num_sp_selected;
+    diffsp = all_eval.(eval_name).all_num_sp_selected(:, 3:5)-all_eval.(eval_name).greedy_it_num_sp_selected;
+    fprintf('====== %s =======\n', all_eval.(eval_name).name)
+    fprintf('Mean improvement gco = %g, gcss = %g, gsss = %g\n', nanmean(diffsp, 1));
+    fprintf('Max improvement gco = %d, gcss = %d, gsss = %d\n', nanmax(diffsp, [], 1));
+%     fprintf('=== Mean Diff before Greedy ===\n');
+    fprintf('Mean diff before gco - gcss = %d, gcss - gsss  = %g, gco - gsss = %g\n', -mean(diff(gsp, 1, 2)),  -mean(diff(gsp(:, [1,3]),1,2)));
+%     fprintf('=== Mean Diff after Greedy ===\n');
+    fprintf('Mean diff after gco - gcss = %d, gcss - gsss  = %g, gco - gsss = %g\n', -mean(diff(gitsp, 1, 2)), -mean(diff(gitsp(:, [1,3]),1,2)));
+
 end
