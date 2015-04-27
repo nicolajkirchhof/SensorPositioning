9066175f5201108e81658bc34893389dc63c7d73
NicolajKirchhof <nicolaj.kirchhof@zuehlke.com>
2015-04-28 01:19:36 +0200

    removed obsolete and changed wcar

--- Sensorplacement/+Experiments/+Diss/+figures/WCAR.m
+++ Sensorplacement/+Experiments/+Diss/+figures/WCAR.m
@@ -1,38 +1,40 @@
-hao =@(x,y) x/ceil(sqrt(2*y+1/4)+1/2);
-hoa =@(x,y) 2*y/x;
+
 %%
 
 eval_names = {'conference_room', 'small_flat', 'large_flat', 'office_floor'};
 opt_names = {'gco_i', 'gcss', 'gsss'};
 close all;
-
+hao =@(x,y) x/ceil(sqrt(2*y+1/4)+1/2);
+hoa =@(x,y) 2*y/x;
 for ideval = 1:numel(eval_names)
     %%
     %     ideval = 1;
     eval_name = eval_names{ideval};
-flt_mspqm = ~cellfun(@isempty, all_eval.(eval_name).mspqm);
-
-haos = cellfun(@(x) hao(numel(x.sensors_selected),numel(x.sc_selected)), all_eval.(eval_name).gco(flt_mspqm));
-% hoas = cellfun(@(x) hoa(numel(x.sensors_selected),numel(x.sc_selected)), all_eval.(eval_name).mspqm(flt_mspqm));
-hreal = cellfun(@(x,y) numel(x.sensors_selected)/numel(y.sensors_selected), all_eval.(eval_name).gco(flt_mspqm), all_eval.(eval_name).mspqm(flt_mspqm));
-hgco = cellfun(@(x) numel(x.sensors_selected), all_eval.(eval_name).gco(flt_mspqm));
-hgcosc = cellfun(@(x) numel(x.sc_selected), all_eval.(eval_name).gco(flt_mspqm));
-hmspqm = cellfun(@(x) numel(x.sensors_selected), all_eval.(eval_name).mspqm(flt_mspqm));
-% hgsss = cellfun(@(x,y) numel(x.sensors_selected)/numel(y.sensors_selected), all_eval.conference_room.gsss(flt_mspqm), all_eval.conference_room.mspqm(flt_mspqm));
-% hgcss = cellfun(@(x,y) numel(x.sensors_selected)/numel(y.sensors_selected), all_eval.conference_room.gcss(flt_mspqm), all_eval.conference_room.mspqm(flt_mspqm));
-
-
-% plot([haos, hoas, hreal])
-% legend('haos', 'hoas', 'hreal');
-% cbar = linspace(0,0.6,3);
-h = plot([haos,  hreal, hgco, hgcosc, hmspqm], 'k');
-set(h(2), 'color', 0.6*ones(1,3));
-% set(h(2), 'linestyle', '-')
-set(h(2), 'linestyle', ':')
-set(h(4), 'linestyle', ':', 'color', 0.8*ones(1,3));
-% set(h(4), 'linestyle', ':')
-% legend('haos', 'hoa', 'hgsss', 'hgcss');
-
+    flt_mspqm = ~cellfun(@isempty, all_eval.(eval_name).mspqm);
+    
+    haos = cellfun(@(x) hao(numel(x.sensors_selected),numel(x.sc_selected)), all_eval.(eval_name).gco(flt_mspqm));
+    % hoas = cellfun(@(x) hoa(numel(x.sensors_selected),numel(x.sc_selected)), all_eval.(eval_name).mspqm(flt_mspqm));
+    hreal = cellfun(@(x,y) numel(x.sensors_selected)/numel(y.sensors_selected), all_eval.(eval_name).gco(flt_mspqm), all_eval.(eval_name).mspqm(flt_mspqm));
+    hgco = cellfun(@(x) numel(x.sensors_selected), all_eval.(eval_name).gco(flt_mspqm));
+    hgcosc = cellfun(@(x) numel(x.sc_selected), all_eval.(eval_name).gco(flt_mspqm));
+    hmspqm = cellfun(@(x) numel(x.sensors_selected), all_eval.(eval_name).mspqm(flt_mspqm));
+    % hgsss = cellfun(@(x,y) numel(x.sensors_selected)/numel(y.sensors_selected), all_eval.conference_room.gsss(flt_mspqm), all_eval.conference_room.mspqm(flt_mspqm));
+    % hgcss = cellfun(@(x,y) numel(x.sensors_selected)/numel(y.sensors_selected), all_eval.conference_room.gcss(flt_mspqm), all_eval.conference_room.mspqm(flt_mspqm));
+    
+    
+    % plot([haos, hoas, hreal])
+    % legend('haos', 'hoas', 'hreal');
+    % cbar = linspace(0,0.6,3);
+    figure;
+%     h = plot([haos,  hreal, hgco, hgcosc, hmspqm], 'k');
+    h = plot([haos, hreal], 'k');
+    set(h(2), 'color', 0.6*ones(1,3));
+    % set(h(2), 'linestyle', '-')
+    set(h(2), 'linestyle', ':')
+%     set(h(4), 'linestyle', ':', 'color', 0.8*ones(1,3));
+    % set(h(4), 'linestyle', ':')
+    % legend('haos', 'hoa', 'hgsss', 'hgcss');
+    
     %%
     filename = sprintf('WCAR%s.tex', eval_name);
     full_filename = sprintf('export/%s', filename);
@@ -41,11 +43,11 @@ set(h(4), 'linestyle', ':', 'color', 0.8*ones(1,3));
         'width', '10cm',...
         'extraCode', '\standaloneconfig{border=0.1cm}',...
         'standalone', true);
-%     find_and_replace(full_filename, 'ylabel={\[\\#\]}', 'ylabel={[\\#SP]},\nevery axis y label/.style={at={(current axis.north west)},anchor=east}');
-%     find_and_replace(full_filename,'bar\ width=\d.\d*cm,', 'bar width=0.8,');
-%     find_and_replace(full_filename,'bar\ shift=.\d.\d*cm,', '');
-%     find_and_replace(full_filename,'bar\ shift=\d.\d*cm,', '');
-%     find_and_replace(full_filename,'inner\ sep=0mm', 'inner sep=1pt');
+    %     find_and_replace(full_filename, 'ylabel={\[\\#\]}', 'ylabel={[\\#SP]},\nevery axis y label/.style={at={(current axis.north west)},anchor=east}');
+    %     find_and_replace(full_filename,'bar\ width=\d.\d*cm,', 'bar width=0.8,');
+    %     find_and_replace(full_filename,'bar\ shift=.\d.\d*cm,', '');
+    %     find_and_replace(full_filename,'bar\ shift=\d.\d*cm,', '');
+    %     find_and_replace(full_filename,'inner\ sep=0mm', 'inner sep=1pt');
     Figures.compilePdflatex(filename, false, false);
 end
 %%
