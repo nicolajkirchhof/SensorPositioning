f0c3d8daa2748e95715045f75dc4c27438d7b383
NicolajKirchhof <nicolaj.kirchhof@zuehlke.com>
2015-06-01 00:52:35 +0200

    Updated Sensormodel

--- Sensorplacement/src/+Figures/Sensormodel.m
+++ Sensorplacement/src/+Figures/Sensormodel.m
@@ -14,8 +14,18 @@ wpn = [2500 750];
 drawPoint(wpn, 'color', 'k', 'marker', 'o', 'markerfacecolor', 'k');
 text(2550, 750, '$W_1$');
 
+spn2 = [2250 0];
+drawPoint(spn2, 'color', 'k', 'marker', 'o', 'markerfacecolor', 'k');
+text(spn2(1)-100, spn2(2)-100, '$S_2$');
+drawEdge([spn2 wpn], 'color', 'k');
+
+s2_ang = rad2deg(angle2Points(spn2, wpn));
+s1_ang = rad2deg(angle2Points([0,0], wpn));
+fov_inner = circleArcToPolyline([wpn, 250, 180+s1_ang, s2_ang-s1_ang], 128);
+
+
 drawEdge([0, 0, wpn], 'color', 'k');
-text(2000, 500, '$d_{1,1}$');
+
 
 fov = circleArcToPolyline([0, 0, 3000, varphi, omega], 128);
 fov = [0,0;fov];
@@ -29,8 +39,12 @@ drawEdge(dist, 'color', 'k', 'linestyle', '-.');
 
 fov_marker = circleArcToPolyline([0, 0, 500, varphi, omega], 128);
 drawPolyline(fov_marker, 'color', 'k');
-text(600, 350, '$\Omega_1$');
+text(500, 300, '$\psi_1$', 'interpreter', 'none');
 text(1300, 600, '$d_1$');
+
+fov_target = circleArcToPolyline([0, 0, 750, 0, s1_ang], 128);
+drawPolyline(fov_target, 'color', 'k');
+
 
 phi = circleArcToPolyline([0, 0, 1000, 0, varphi], 128);
 x_axis = [0,0,3000, 0];
@@ -38,14 +52,25 @@ y_axis = [0,0,0,1800];
 drawEdge(x_axis, 'k');
 drawEdge(y_axis, 'k');
 drawPolyline(phi, 'k');
-text(2750, -100, '$x \rightarrow$');
-text(-200, 1750,  '$\uparrow$ \\ $y$');
-text(1050, 100,  '$\varphi$');
-text(-200, -100,  '$S_1=(x,y,\varphi)^T$');
+text(2750, -100, '$x \rightarrow$', 'interpreter', 'none');
+text(-200, 1750,  '$\uparrow$ \\ $y$', 'interpreter', 'none');
+text(1050, 100,  '$\varphi_1$', 'interpreter', 'none');
+text(800, 100,  '$\beta_{1,1}$', 'interpreter', 'none');
+text(-200, -100,  '$S_1=(x,y,\varphi)^T$', 'interpreter', 'none');
+drawPolyline(fov_inner, 'color', 'k');
+text(2100, 500, '$\gamma_{1,2,1}$', 'interpreter', 'none');
 
 plot(0,0, 'marker', 'o', 'markersize', 6, 'markerfacecolor', 'k',  'markeredgecolor', 'k');
 %%
-Figures.makeFigure('Sensormodel');
+filename = 'Sensormodel.tex';
+full_filename = sprintf('export/%s', filename);
+matlab2tikz(full_filename, 'parseStrings', false,...
+    'height', '6cm',...
+    'width', '11cm',...
+    'extraCode', '\standaloneconfig{border=0.1cm}',...
+    'standalone', true);
+Figures.compilePdflatex(filename, true, false);
+% Figures.makeFigure('Sensormodel');
 % filename = '../../Dissertation/Thesis/Figures/Sensormodel.tikz';
 % matlab2tikz(filename, 'parseStrings', false,...
 %     'tikzFileComment', '% -*- root: TestingFigures.tex -*-', 'width', '11cm',...
--- Sensorplacement/src/+Figures/SimplePolygonExample.m
+++ Sensorplacement/src/+Figures/SimplePolygonExample.m
@@ -14,11 +14,11 @@ hole = [300 300 400 400 100 100 300;
     
 poly = bpolyclip(poly, hole, 0, 1);       
 mb.drawPolygon(poly, 'color', 'k', 'linewidth', 3);
-mb.drawPoint(poly{1}{1}(:,[1,2,3,5,6]), 'color', 'k', 'markersize', 8, 'markerfacecolor', 'k');
+mb.drawPoint(poly{1}{1}(:,[4]), 'color', 'k', 'markersize', 8, 'markerfacecolor', 'k');
 mb.drawPoint(poly{1}{2}(:,[2,3,4,5,6]), 'color', 'k', 'markersize', 8, 'markerfacecolor', 'k');
 gray = 0.6;
 mb.drawPoint(poly{1}{2}(:,[1]), 'color', gray*ones(1,3), 'markersize', 8, 'markerfacecolor', gray*ones(1,3));
-mb.drawPoint(poly{1}{1}(:,[4]), 'color', gray*ones(1,3), 'markersize', 8, 'markerfacecolor', gray*ones(1,3));
+mb.drawPoint(poly{1}{1}(:,[1,2,3,5,6]), 'color', gray*ones(1,3), 'markersize', 8, 'markerfacecolor', gray*ones(1,3));
 
 text(600, 300, '$r_1$');
 text(200, 250, '$h_1$');
@@ -53,7 +53,7 @@ matlab2tikz(full_filename, 'parseStrings', false,...
     'extraCode', '\standaloneconfig{border=0.1cm}',...
     'standalone', true);
 % Figures.compilePdflatex(filename, false);
-Figures.compilePdflatex(filename, true, true);
+Figures.compilePdflatex(filename, true, false);
 
 
 % matlab2tikz('export/SimplePolygonExample.tex')
